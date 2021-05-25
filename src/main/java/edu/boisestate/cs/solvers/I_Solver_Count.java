781de216e4dc9c394e9ff4f10186c938fce8e865
Phillip <phillip.bruce@hey.com>
2021-05-25 12:23:44 -0700

    Added I_Solver_Count

---
+++ src/main/java/edu/boisestate/cs/solvers/I_Solver_Count.java
@@ -0,0 +1,38 @@
+package edu.boisestate.cs.solvers;
+
+import java.util.Set;
+
+import edu.boisestate.cs.automatonModel.A_Model;
+
+public interface I_Solver_Count<T extends A_Model<T>> {
+
+	/**
+	 * Get the number of solutions represented by the symbolic string model.
+	 *
+	 * @param id the identifier of the symbolic string
+	 *
+	 * @return number of solutions for a given node in the graph
+	 */
+	long getModelCount(int id);
+
+//    /**
+//     * Get a single string value if it exists from the solutions represented by
+//     * the symbolic string.
+//     *
+//     * @param id
+//     *         the identifier of the symbolic string
+//     *
+//     * @return the single string value if it exists, otherwise null.
+//     */
+//    String getSingleValue(int id);
+
+	/**
+	 * Enumerates all possible values for a given symbolic string
+	 *
+	 * @param id the identifier of the symbolic string
+	 *
+	 * @return a set of all possible string values represented by the symbolic
+	 *         string
+	 */
+	Set<String> getAllVales(int id);
+}
