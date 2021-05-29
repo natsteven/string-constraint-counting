1abe2e4a264d99eada8cf3f3434c5010818e6b81
Phillip <phillip.bruce@hey.com>
2021-05-29 16:45:00 -0700

    Added I_Model_Manager

---
+++ src/main/java/edu/boisestate/cs/automatonModel/A_Model_Manager.java
@@ -0,0 +1,5 @@
+package edu.boisestate.cs.automatonModel;
+
+public abstract class A_Model_Manager {
+
+}
---
+++ src/main/java/edu/boisestate/cs/automatonModel/I_Model_Manager.java
@@ -0,0 +1,41 @@
+package edu.boisestate.cs.automatonModel;
+
+import edu.boisestate.cs.Alphabet;
+
+public interface I_Model_Manager<T extends A_Model<T>> {
+
+	Alphabet getAlphabet();
+
+	/**
+	 * Create a new automaton model from a concrete string
+	 * 
+	 * @param string
+	 * @return
+	 */
+	T createString(String string);
+
+	/**
+	 * Create a new symbolic string from 0 to up to a certain length
+	 * 
+	 * @param initialBound the upper bound of the lenght (inlcusive)
+	 * @return
+	 */
+	T createAnyString(int initialBound);
+
+	/**
+	 * A string with no upper bound - for unbounded models
+	 * 
+	 * @return
+	 */
+	T createAnyString();
+
+	/**
+	 * Creates a symbolic string with length from min to max (both inclusive)
+	 * 
+	 * @param min
+	 * @param max
+	 * @return
+	 */
+	T createAnyString(int min, int max);
+
+}
